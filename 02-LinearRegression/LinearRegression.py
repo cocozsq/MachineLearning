c49d7b50706485b7f9dd04d02227cdf66f6a59f5
zhshiqi <3024197649@qq.com>
2018-11-17 21:51:42 +0800

    add gradient descent

--- 02-LinearRegression/LinearRegression.py
+++ 02-LinearRegression/LinearRegression.py
@@ -27,6 +27,49 @@ class LinearRegression:
 		self.coef_ = self._theta[1:]
 
 		return self
+	# 梯度下降法求解线性回归模型模型参数
+	def fit_gd(self, X_train, y_train, eta=0.01, n_iters = 1e4):
+		assert X_train.shape[0] == y_train.shape[0], "the size of X_train must be equal to the size of y_train"
+
+		# 损失函数 MSE
+		def J(theta, X_b, y):
+			try:
+				return np.sum((y-X_b.dot(theta))**2) / len(y)
+			except:
+				return float('inf')
+		# 偏导数，
+		def dJ(theta, X_b, y):
+			# res = np.empty(len(theta))
+			# res[0] = np.sum(X_b.dot(theta) - y)
+			# for i in range(1, len(theta)):
+			# 	res[i] = (X_b.dot(theta) - y).dot(X_b[:, i])
+			# return res*2/len(X_b)
+			return X_b.T.dot(X_b.dot(theta) - y) * 2./len(X_b)
+
+		# 梯度下降过程
+		def gradient_descent(X_b, y, initial_theta, eta, n_iters = 1e4, epsilon=1e-6):
+
+			theta = initial_theta
+			cur_iter = 0 # 迭代次数
+
+			while cur_iter < n_iters:
+				gradient = dJ(theta, X_b, y)
+				last_theta = theta
+				theta = theta - eta*gradient
+				if abs(J(theta, X_b, y) - J(last_theta, X_b, y)) < epsilon:
+					break
+				cur_iter += 1
+			return theta
+
+		X_b = np.column_stack([np.ones((len(X_train), 1)), X_train])
+		initial_theta = np.zeros(X_b.shape[1])
+
+		self._theta =gradient_descent(X_b, y_train, initial_theta, eta, n_iters)
+		self.intercept_ = self._theta[0]
+		self.coef_ = self._theta[1:]
+		return self
+
+
 
 	def predict(self, X_predict):
 		
